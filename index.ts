b0647b9c6763e04778ea910c404592e0ab165f97
Yoana <yoanabarbie@icloud.com>
2024-06-24 17:26:40 +0200

    fix: first exercise done

--- index.ts
+++ index.ts
@@ -3,20 +3,30 @@
 class Persona {
     protected nombre: string
     protected edad: number
-    protected DNI: string[]
+    protected DNI: string
     protected sexo: (string | string)
     protected peso: number
     protected altura: number
 
-    constructor(nombre:string, edad: number, DNI: string[],) {
+    constructor(nombre: string = '',
+        edad: number = 0,
+        DNI: string = '',
+        sexo: string | string,
+        peso: number = 0,
+        altura: number = 0
+    )  {
         this.nombre = nombre;
         this.edad = edad;
         this.DNI = DNI;
-        this.sexo = sexo
-        this.peso = peso
-        this.altura = altura
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
     }
 }
+
+const persona1 = new Persona('Yoana', 28, '442234Y', 'M', 55, 175);
+const persona2 = new Persona('Elena', 28, '12345678M', 'M', 60, 165);
+const persona3 = new Persona('Ivan', 35, '87654321J', 'H', 75, 180);
 
 // RETO 2
 
@@ -28,8 +38,12 @@ class Password {
         this.longitud = longitud
         this.contrasena = contrasena
     }
-}
 
+    public randomPass() {
+        return
+    }
+}
+const Contrase = new Password('longitud', '12345')
 // RETO 3
 
 class Electrodomestico {
@@ -38,10 +52,10 @@ class Electrodomestico {
     consumo: string
     peso: number
 
-    constructor(precioBase: number, color:string, consumo:string, peso:number){
+    constructor(precioBase: number, color: string, consumo: string, peso: number) {
         this.precioBase = precioBase
         this.color = color
         this.consumo = consumo
-        this.peso= peso
+        this.peso = peso
     }
 }
